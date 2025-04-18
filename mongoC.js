04cd8f9962c665dacc35ee2fc7a4db1454977b95
IT21786464 <97947640+TNjo@users.noreply.github.com>
2025-04-19 00:27:52 +0530

    deploy to ec2

--- mongoC.js
+++ mongoC.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-// const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
+const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
 const connectionString = `mongodb+srv://tnpjcrew:MYscK7gH8T:U:mb@devcluster.cyvgckn.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster`; // clustore url
 const client = new MongoClient(connectionString);
 let conn;
